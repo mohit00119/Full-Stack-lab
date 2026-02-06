3e80e9c82d65f15cda983d30f4f921c3e73bc1eb
mohit00119 <mohitbhagour66@gmail.com>
2026-02-06 13:45:29 +0530

    lecture-10

---
+++ condition.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Enter a number: "
+read n
+if [ $n -gt 0 ]; then
+    echo "The number is positive."
+elif [ $n -lt 0 ]; then
+    echo "The number is negative."
+else
+    echo "The number is zero."
+fi
---
+++ hello.sh
@@ -0,0 +1,5 @@
+#this is a comment
+echo "Hello "
+    read n
+            
+#!/bin/bash
